b2498a11fd21dde9d046fe6d2407f0a8d501c9b2
Owajeul Islam <owajeul.islam@brainstation-23.com>
2025-05-02 10:10:25 +0600

    add UserPurchaseReportDto for admin reporting

---
+++ eBookStore.Application/DTOs/UserPurchaseReportDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eBookStore.Application.DTOs
+{
+    public class UserPurchaseReportDto
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string MostPurchasedGenre { get; set; }
+        public decimal TotalSpend { get; set; }
+        public int TotalBooksPurchased { get; set; }
+    }
+
+}
